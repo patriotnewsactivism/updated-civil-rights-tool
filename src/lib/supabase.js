e506579085fc562f10c6d4f5e216a6e9f500ddd6
Matthew Reardon <patriotnewsactivism@proton.me>
2026-04-24 19:04:06 -0500

    fix: point supabase client to correct project vrdnrbjnitptxrexdlao

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xvvdelxlwjfazksvneom.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2dmRlbHhsd2pmYXprc3ZuZW9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2NDgwNDQsImV4cCI6MjA2OTIyNDA0NH0.yN9_-zEuMIorZxUNBCdE1o8haP59imBp-lLKXPbu8J8';
+const supabaseUrl = 'https://vrdnrbjnitptxrexdlao.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyZG5yYmpuaXRwdHhyZXhkbGFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc1MzU2MjUsImV4cCI6MjA4MzExMTYyNX0.ve-Ye_ZxWRmeAB4p3YMvl2zoOkmOV65m2YWcwhXStHY';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
